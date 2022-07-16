b518aa22af9ba6454e90de5246f6e98c7064178f
josueuni28 <josueunisempre@gmail.com>
2022-07-16 13:29:33 -0300

    Melhorias e novas funções Express

--- exp/josueApp/routes/users.js
+++ exp/josueApp/routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const Title = 'Users'
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users', { title: Title });
+});
+
+router.get('/:id', function(req, res, next) {
+  res.render('user', { title: Title, user: req.params.id });
 });
 
 module.exports = router;
